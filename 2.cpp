4d29027e5cc1818d6eeb5c966125f6c95689e320
lytvynenko98a <93118800+lytvynenko98a@users.noreply.github.com>
2021-10-25 10:36:31 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+#include <ctime> 
+
+using namespace std;
+
+
+int main()
+{
+    srand(time(NULL));
+
+    int** M;
+    int const SIZE = 5;
+    M = new int* [SIZE];
+    for (int i = 0; i < 5; i++)
+        M[i] = new int[SIZE];
+
+
+    for (int i = 0; i < 5; i++)
+        for (int n = 0; n < 5; n++)
+            M[i][n] = rand() % 10;
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int n = 0; n < 5; n++)
+            cout << M[i][n] << " ";
+        cout << endl;
+    }
+    system("pause");
+
+    return 0;
+    //prosto matrica :(
+}
